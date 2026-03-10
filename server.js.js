0ed07d24051453fc4b8f50f15d2f526794036cf7
CarlosYes <161829023+CarlosYes@users.noreply.github.com>
2026-03-10 19:40:21 +0100

    Add files via upload

---
+++ server.js.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*", methods: ["GET", "POST"] }
+});
+
+let activeUsers = new Map();
+
+io.on('connection', (socket) => {
+    socket.on('update_position', (userData) => {
+        activeUsers.set(socket.id, { ...userData, id: socket.id });
+        io.emit('radar_update', Array.from(activeUsers.values()));
+    });
+
+    socket.on('disconnect', () => {
+        activeUsers.delete(socket.id);
+        io.emit('radar_update', Array.from(activeUsers.values()));
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Radar RAU Online na porta ${PORT}`));
